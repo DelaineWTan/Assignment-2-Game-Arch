d257b1edcaa973c697796e5b5250b8d29d5a66e4
Delaine Tan <dtan32@se12l0325-22.ad.bcit.ca>
2024-02-20 16:21:06 -0800

    Fulfill requirement 1: generate maze with different textures for walls and floor.

--- Assignment 2/GameViewController.swift
+++ Assignment 2/GameViewController.swift
@@ -2,7 +2,7 @@
 //  GameViewController.swift
 //  Assignment 2
 //
-//  Created by Faiz Hassany on 2024-02-17.
+//  Created by Delaine Tan on 2024-02-20.
 //
 
 import UIKit
@@ -10,20 +10,13 @@ import QuartzCore
 import SceneKit
 
 class GameViewController: UIViewController {
-
+    let scene = SCNScene(named: "art.scnassets/main.scn")!
+    var rotAngle = 0.0 // Keep track of crate rotation angle
+    let mazeRows = 5;
+    let mazeCols = 5;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // create a new scene
-        let scene = SCNScene(named: "art.scnassets/main.scn")!
-        
-        // create and add a camera to the scene
-        let cameraNode = SCNNode()
-        cameraNode.camera = SCNCamera()
-        scene.rootNode.addChildNode(cameraNode)
-        
-        // place the camera
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
         
         // create and add a light to the scene
         let lightNode = SCNNode()
@@ -38,12 +31,6 @@ class GameViewController: UIViewController {
         ambientLightNode.light!.type = .ambient
         ambientLightNode.light!.color = UIColor.darkGray
         scene.rootNode.addChildNode(ambientLightNode)
-        
-        // retrieve the ship node
-        let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
-        
-        // animate the 3d object
-        ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
         
         // retrieve the SCNView
         let scnView = self.view as! SCNView
@@ -63,7 +50,19 @@ class GameViewController: UIViewController {
         // add a tap gesture recognizer
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
         scnView.addGestureRecognizer(tapGesture)
+        
+        // Create rotating crate at 0,0 (start of maze)
+        addCube()
+        reanimate()
+        
+        // Create the maze node
+        let mazeNode = createMazeNode()
+        
+        // Add the maze node to the scene
+        scene.rootNode.addChildNode(mazeNode)
     }
+    
+    
     
     @objc
     func handleTap(_ gestureRecognize: UIGestureRecognizer) {
@@ -112,5 +111,109 @@ class GameViewController: UIViewController {
             return .all
         }
     }
+    
+    enum Direction: Int {
+        case north = 0
+        case east
+        case south
+        case west
+    }
+    
+    func createMazeNode() -> SCNNode {
+        let mazeNode = SCNNode()
+        
+        var maze = Maze(Int32(mazeRows), Int32(mazeCols))
+        maze.Create()
+        
+        // Define the size of a single cell, padding, and wall thickness
+        let cellSize: CGFloat = 1.0
+        let wallThickness: CGFloat = 0.1
+        // Adjust the position of the walls to avoid perfect overlap
+        let wallOffset: Float = 0.05 // Adjust as needed
 
+        // Load the floor texture image
+        let floorTexture = UIImage(named: "floor.png") // Replace "floorTexture.jpg" with the actual name of your floor texture image
+        
+        // Iterate through maze cells
+        for row in 0..<maze.rows {
+            for col in 0..<maze.cols {
+                let cell = maze.GetCell(row, col)
+                let cellPosition = SCNVector3(CGFloat(col) * (cellSize), 0, CGFloat(row) * (cellSize))
+                
+                // Create a node for the cell
+                let cellGeometry = SCNBox(width: cellSize, height: 0.0, length: cellSize, chamferRadius: 0)
+                let cellNode = SCNNode(geometry: cellGeometry)
+                cellNode.position = SCNVector3(CGFloat(col) * cellSize, -0.5, CGFloat(row) * cellSize)
+
+                // Apply floor texture to the cell
+                let material = SCNMaterial()
+                material.diffuse.contents = floorTexture
+                cellNode.geometry?.firstMaterial = material
+                
+                mazeNode.addChildNode(cellNode)
+                
+                // Check walls and create them if present
+                if cell.northWallPresent {
+                    // Create north wall with offset
+                    let northWallNode = createWall(position: SCNVector3(cellPosition.x, Float(wallThickness)/2, cellPosition.z - Float(cellSize/2) - wallOffset),
+                                                   width: cellSize, height: 1, length: wallThickness, textureName: "north.png", inScene: scene)
+                    mazeNode.addChildNode(northWallNode)
+
+                }
+                if cell.southWallPresent {
+                    // Create south wall with offset
+                    let southWallNode = createWall(position: SCNVector3(cellPosition.x, Float(wallThickness)/2, cellPosition.z + Float(cellSize)/2 + wallOffset),
+                                                   width: cellSize, height: 1, length: wallThickness, textureName: "south.jpeg", inScene: scene)
+                    mazeNode.addChildNode(southWallNode)
+                }
+                if cell.eastWallPresent {
+                    // Create east wall with offset
+                    let eastWallNode = createWall(position: SCNVector3(cellPosition.x + Float(cellSize)/2 + wallOffset, Float(wallThickness)/2, cellPosition.z),
+                                                  width: wallThickness, height: 1, length: cellSize, textureName: "east.jpeg", inScene: scene)
+                    mazeNode.addChildNode(eastWallNode)
+                }
+                if cell.westWallPresent {
+                    // Create west wall with offset
+                    let westWallNode = createWall(position: SCNVector3(cellPosition.x - Float(cellSize)/2 - wallOffset, Float(wallThickness)/2, cellPosition.z),
+                                                  width: wallThickness, height: 1, length: cellSize, textureName: "west.jpeg", inScene: scene)
+                    mazeNode.addChildNode(westWallNode)
+                }
+            }
+        }
+        
+        return mazeNode
+    }
+    
+    func createWall(position: SCNVector3, width: CGFloat, height: CGFloat, length: CGFloat, textureName: String, inScene scene: SCNScene) -> SCNNode {
+        let wallNode = SCNNode(geometry: SCNBox(width: width, height: height, length: length, chamferRadius: 0))
+        let material = SCNMaterial()
+        material.diffuse.contents = UIImage(named: textureName)
+        wallNode.geometry?.firstMaterial = material
+        wallNode.position = position
+        return wallNode
+    }
+    
+    
+    func addCube() {
+        let theCube = SCNNode(geometry: SCNBox(width: 1, height: 1, length: 1, chamferRadius: 0)) // Create a object node of box shape with width of 1 and height of 1
+        theCube.name = "The Cube" // Name the node so we can reference it later
+        theCube.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "crate.jpg") // Diffuse the crate image material across the whole cube
+        theCube.position = SCNVector3(0, 0, 0) // Put the cube at position (0, 0, 0)
+        scene.rootNode.addChildNode(theCube) // Add the cube node to the scene
+    }
+    
+    @MainActor
+    func reanimate() {
+        let theCube = scene.rootNode.childNode(withName: "The Cube", recursively: true) // Get the cube object by its name (This is where line 40 comes in)
+        rotAngle += 0.0005 // Increment rotation of the cube by 0.0005 radians
+        // Keep the rotation angle in the range of 0 and pi
+        if rotAngle > Double.pi {
+            rotAngle -= Double.pi
+        }
+        theCube?.eulerAngles = SCNVector3(0, rotAngle, 0) // Rotate cube by the final amount
+        // Repeat increment of rotation every 10000 nanoseconds
+        Task { try! await Task.sleep(nanoseconds: 10000)
+            reanimate()
+        }
+    }
 }
